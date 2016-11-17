400d9d4cdf8220e330ac401f49fa0243e5d9e06a
John Dave B. Salcedo <davesalcedo21@gmail.com>
2016-11-17 09:10:07 +0800

    Latest Exercise5

--- exercise5/exercise5.php
+++ exercise5/exercise5.php
@@ -1,17 +1,4 @@
-<?php
-include_once 'dbconfig.php';
-
-// delete condition
-if(isset($_GET['delete_id']))
-{
- $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
- mysqli_query($link, $sql_query);
- header("Location: $_SERVER[PHP_SELF]");
-}
-// delete condition
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 <head>
 <script>
@@ -49,7 +36,7 @@ p {
 	}
 	
 div {
-    border: 5px solid black;
+    border: 2pxpx solid black;
     background-color: #F2F3F4;
     padding-top: 20px;
     padding-right: 20px;
@@ -58,7 +45,7 @@ div {
 }
 
 body {
-background-image: url("background.jpg")
+background-image: url("bg.jpg")
 }
 
 a {
@@ -71,19 +58,18 @@ table {
 }
 
 th, td {
-    padding: 15px;
+    padding: 10px;
     text-align: left;
-    border-bottom: 3px solid #000000;
-	font-size: 20px;
+    border-bottom: 2px solid #000000;
 }
 </style>
 </head>
 <body>
 
-<h1>Exercise 5</h1>
+<h1>Exercise 2</h1>
 <p>Welcome to my Home Page.</p>
 
-
+<div>
 <table>
   <tr>
     <th>Firstname</th>
@@ -98,12 +84,12 @@ th, td {
     <td><p><b><a href="www.youtube.com" target="_blank">Youtube</a></b></p></td>
   </tr>
 </table>
-
+</div>
 
 <h1>Ask anything about myself.</h1>
 <p id="demo"></p>
 
-
+<div>
 <table>
   <tr>
     <th>What is my full name?</th>
@@ -120,7 +106,7 @@ th, td {
 	<td><button type="button" onclick="myFunction5()">Try it</button></td>
   </tr>
 </table>
-
+</div>
 
 <?php
 // define variables and set to empty values
@@ -179,65 +165,41 @@ function test_input($data) {
 }
 ?>
 
-<script type="text/javascript">
-function edt_id(id)
-{
- if(confirm('Sure to edit ?'))
- {
-  window.location.href='edit_data.php?edit_id='+id;
- }
-}
-function delete_id(id)
-{
- if(confirm('Sure to Delete ?'))
- {
-  window.location.href='index.php?delete_id='+id;
- }
-}
-</script>
-</head>
-<body>
-<center>
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
 
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 
-
-    <table align="center">
-    <tr>
-	
-    <th colspan="8"><center><a href="add_data.php">Add data here.</a></th>
-    
-	</tr>
-   <th>Name</th>
-    <th>Nickname</th>
-    <th>Email</th>
-	<th>Address</th>
-	<th>Gender</th>
-	<th>Cellphone Number</th>
-	
-    <th colspan="2">Operations</th>
-    </tr>
-    <?php
- $sql_query="SELECT * FROM users";
- $result_set=mysqli_query($link, $sql_query);
- while($row=mysqli_fetch_row($result_set))
- {
-  ?>
-        <tr>
-        <td><?php echo $row[1]; ?></td>
-        <td><?php echo $row[2]; ?></td>
-        <td><?php echo $row[3]; ?></td>
-		<td><?php echo $row[4]; ?></td>
-		<td><?php echo $row[5]; ?></td>
-		<td><?php echo $row[6]; ?></td>
-		
-  <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
-        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
-        </tr>
-        <?php
- }
- ?>
-    </table>
-    </div>
 
 
 
