11e614c97b984003e1c5aa98b4e0c056335078a9
John Dave B. Salcedo <davesalcedo21@gmail.com>
2016-12-01 10:18:08 +0800

    i change mypage to index

--- exercise5/index.php
+++ exercise5/index.php
@@ -1,6 +1,5 @@
 <?php
 include_once 'dbconfig.php';
-
 // delete condition
 if(isset($_GET['delete_id']))
 {
@@ -10,16 +9,138 @@ if(isset($_GET['delete_id']))
 }
 // delete condition
 ?>
- <a href = "mypage.php" > <center><h2> Back to main page</h2></a>
- 
-  <body style= 'background-image: url("background.png")'>
-  
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "My full name is John Dave Baylon Salcedo.";
+	}
+	function myFunction2() {
+    document.getElementById("demo").innerHTML = "I lived at Villamor Air Base Pasay City.";
+	}
+	function myFunction3() {
+    document.getElementById("demo").innerHTML = "I am 18 years old.";
+	}
+	function myFunction4() {
+    document.getElementById("demo").innerHTML = "I used to collect Shoes especially Vans and I play different genre of games.";
+	}
+	function myFunction5() {
+    document.getElementById("demo").innerHTML = "Anything except vegetables.";
+	}
+</script>
+<style>
+
+.error {color: #FF0000;}
+h1 {
+    font-style: italic;
+	font-size: 50px;
+	 color: white;
+	}
+	
+
+body {
+    background: url("background.png");
+    background-size: 2500px 2500px;
+    background-repeat: no-repeat;
+    padding-top: 40px;
+}
+
+a {
+	font-size: 20px;
+	color: red;
+	}
+	
+p {
+	font-size: 20px;
+	color:orange;
+	}
+	
+table {
+    border-collapse: collapse;
+    width: 50%;
+}
+
+th, td {
+    text-align: left;
+    padding: 15px;
+    border-bottom: 1px solid #000000;
+}
+
+</style>
+</head>
+<body>
+<h1><center>Exercise 5</h1>
+<center>
+<a href="mypage.php" target="_self">HOME</a> |
+<a href="info.php" target="_self">INFO</a> |
+<a href="askme.php" target="_parent">ASK ME</a>  </center>
+<p><center>Welcome to my Home Page.</p>|
+<img src="cat.gif" style="width:420px;height:250px;">
+<img src="head.gif" style="width:420px;height:250px;">
+<img src="pikachu.gif" style="width:420px;height:250px;">
+
+
+
+
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
 <script type="text/javascript">
 function edt_id(id)
 {
@@ -40,19 +161,15 @@ function delete_id(id)
 <body>
 <center>
 
-<div id="header">
- <div id="content">
-    <label>Welcome <a href="http://cleartuts.blogspot.com" target="_blank"></a></label>
-    </div>
-</div>
 
-<div id="body">
- <div id="content">
+
     <table align="center">
     <tr>
-    <th colspan="8"><a href="add_data.php">add data here.</a></th>
-    </tr>
-    <th>Name</th>
+	
+    <th colspan="8"><center><a href="add_data.php">Add data here.</a></th>
+    
+	</tr>
+   <th>Name</th>
     <th>Nickname</th>
     <th>Email</th>
 	<th>Address</th>
@@ -83,8 +200,9 @@ function delete_id(id)
  ?>
     </table>
     </div>
-</div>
 
-</center>
+<h4>-Dave Salcedo-</h4>
+
 </body>
 </html>
+
